211c981cbc2059d7d38c7d103b293a25f1de675d
albert abril <albert.abril@gmail.com>
2014-05-06 19:35:18 +0200

    initial commit mine

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var jade = require('gulp-jade');
+
+gulp.task('templates', function(){
+  var YOUR_LOCALS = {};
+
+  gulp.src("./lib/*jade")
+  .pipe(jade({
+    locals: YOUR_LOCALS
+  }))
+  .pipe(gulp.dest('./dist/'))
+
+});
